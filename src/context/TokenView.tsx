cbc70c9623f9aa88679286c0dca519ba6b0a3b08
cooolman2008 <cooolman2008@gmail.com>
2024-04-04 02:24:00 +1100

    feat: reduce the function call cost

--- src/context/TokenView.tsx
+++ src/context/TokenView.tsx
@@ -50,7 +50,7 @@ function TokenView({ params }: { params: { slug: `0x${string}` } }) {
           <button
             onClick={() =>
               write({
-                value: parseEther("0.01"),
+                value: parseEther("0.0001"),
               })
             }
             className="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 me-2 mb-2"
